bf7f2d0922deb788e039bbb5b9f17375c2baf074
Koketso Seima <69907917+KOKETSO23@users.noreply.github.com>
2024-11-06 08:33:51 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace textfile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string DocumentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            string filepath = Path.Combine(DocumentPath, "New.txt");
+
+                 Console.WriteLine("Please Enter your name");
+                 string Texttowrite = Console.ReadLine();
+
+            using (StreamWriter writer = new StreamWriter(filepath))
+            {
+                writer.WriteLine(Texttowrite);
+            }
+
+            Console.WriteLine("Successfully");
+        }
+
+
+    }
+}
